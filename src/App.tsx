749636413033f170cf3881665e19f8e1bb8ffe40
mikolajjkrol <mikolajkock@gmail.com>
2025-11-05 13:41:33 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+
+function App() {
+
+  return (
+    <div className="app dark">
+      <nav className="navbar">
+        
+      </nav>
+      <div className="container">
+        <div className="sidebar">
+
+        </div>
+        <div className="projects">
+          <div className="content">
+            Your booking systems
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
